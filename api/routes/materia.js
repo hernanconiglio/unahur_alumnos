f1c4bc72b2ce7741a2f653e40696ef9f0b90b123
hernanconiglio <hernanconiglio@gmail.com>
2022-09-11 19:35:03 -0300

    arreglo metodo put de materia

--- api/routes/materia.js
+++ api/routes/materia.js
@@ -49,8 +49,8 @@ router.put("/:id", (req, res) => {
   const onSuccess = materia =>
     materia
       .update(
-        { nombre: req.body.nombre }, { fields: ["nombre"] }, 
-        { id_carrera: req.body.id_carrera }, { fields: ["id_carrera"] })
+        { nombre: req.body.nombre, id_carrera: req.body.id_carrera } , { fields: ["nombre", "id_carrera"] }
+      )
       .then(() => res.sendStatus(200))
       .catch(error => {
         if (error == "SequelizeUniqueConstraintError: Validation error") {
